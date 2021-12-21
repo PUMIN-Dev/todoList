83cf6b92bbc646b58ead083b95e53c893d054626
PUMIN-Dev <ampumin@gmail.com>
2021-12-21 14:12:20 +0700

    kk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddToDo from "./components/AddToDo.js";
 import SearchBar from "./components/SearchBar";
 import RemainingMessage from "./components/RemainingMessage";
 import ToDoList from "./components/ToDoList";
+import axios from "axios"
+
 // import EditToDo from "./components/EditToDo.js";
+
 
 const initialToDoList = [
 	{ id: uuidv4(), title: "Watching a movie", completed: false },
@@ -12,82 +15,119 @@ const initialToDoList = [
 	{ id: uuidv4(), title: "Dinner with Boss", completed: true },
 ];
 
+
+
 function App(props) {
 	// STATE DECLARATION
 
-	const [toDoList, setToDoList] = useState(initialToDoList);
+	const [toDoList, setToDoList] = useState([]);
 	const [searchText, setSearchText] = useState("");
 	const [searchStatus, setSearchStatus] = useState("");
 	// FUNCTION DECLARATION
 
-	const createToDo = (title) => {
-		const nextToDo = [
-			{ id: uuidv4(), title: title, completed: false },
-			...toDoList,
-		];
-		setToDoList(nextToDo);
+	useEffect(() => {
+		axios.get('http://localhost:8080/todos').then(res => {
+			console.log(res.data)
+			setToDoList(res.data.todos)
+		})
+	}, [])
+
+	const createToDo = title => {
+		axios
+			.post("http://localhost:8080/todos", {
+				title: title,
+				completed: false,
+			})
+			.then(res => {
+				console.log(res.data);
+				const nextToDo = [res.data.todo, ...toDoList];
+				setToDoList(nextToDo);
+			});
 	};
 
-	const deleteToDo = (id) => {
+	// const deleteToDo = (id) => {
+	// 	const idx = toDoList.findIndex((item) => item.id === id);
+	// 	const newToDoList = [...toDoList];
+	// 	if (idx !== -1) {
+	// 		newToDoList.splice(idx, 1);
+	// 	}
+	// 	setToDoList(newToDoList);
+	// };
+
+	const deleteToDo = async id => {
+		const res = await axios.delete(`http://localhost:8080/todos/${id}`)
+		console.log(res.data)
 		const idx = toDoList.findIndex((item) => item.id === id);
 		const newToDoList = [...toDoList];
 		if (idx !== -1) {
 			newToDoList.splice(idx, 1);
 		}
 		setToDoList(newToDoList);
+
 	};
 
-	const updateToDo = (id, { id: objId, ...value }) => {
-		const idx = toDoList.findIndex((item) => item.id === id);
+	// const updateToDo = (id, { id: objId, ...value }) => {
+	// 	// console.log({ id: objId, ...value })
+	// 	const idx = toDoList.findIndex((item) => item.id === id);
+	// 	const newToDoList = [...toDoList];
+	// 	if (idx !== -1) {
+	// 		newToDoList[idx] = { ...newToDoList[idx], ...value };
+	// 	}
+	// 	setToDoList(newToDoList);
+	// };
+	const updateToDo = async (id, { id: objId, ...value }) => {
+		const idx = toDoList.findIndex(item => item.id === id);
 		const newToDoList = [...toDoList];
 		if (idx !== -1) {
-			newToDoList[idx] = { ...newToDoList[idx], ...value };
+		 newToDoList[idx] = { ...newToDoList[idx], ...value };
+		 axios.put(`http://localhost:8080/todos/${id}`, newToDoList[idx]);
 		}
 		setToDoList(newToDoList);
-	};
+	   };
 
-	const pendingToDoList = toDoList.filter((item) => !item.completed);
 
-	const filteredToDoList = toDoList.filter(
-		(item) =>
-			item.title.toLowerCase().includes(searchText.toLowerCase()) &&
-			(searchStatus === "" || item.completed === searchStatus)
-	);
+		const pendingToDoList = toDoList.filter((item) => !item.completed);
 
-	//////Edit////
-	const editToDo = (id, value) => {
-		const idx = toDoList.findIndex((item) => item.id === id);
-		const newToDoList = [...toDoList];
-		if (idx !== -1) {
-			newToDoList[idx] = { ...newToDoList[idx], ...value };
-		}
-		setToDoList(newToDoList);
-	};
-	/////Edit/////
-	return (
-		<div className="container">
-			<div className="mt-5 mx-auto mw-xs">
-				<AddToDo createToDo={createToDo} />
-				<SearchBar
-					searchStatus={searchStatus}
-					setSearchStatus={setSearchStatus}
-					searchText={searchText}
-					setSearchText={setSearchText}
-				/>
-				<RemainingMessage
-					remaining={pendingToDoList.length}
-					total={toDoList.length}
-				/>
-				<ToDoList
-					toDoList={filteredToDoList}
-					setToDoList={setToDoList}
-					deleteToDo={deleteToDo}
-					updateToDo={updateToDo}
-					editToDo={editToDo}
-				/>
+		const filteredToDoList = toDoList.filter(
+			(item) =>
+				item.title.toLowerCase().includes(searchText.toLowerCase()) &&
+				(searchStatus === "" || item.completed === searchStatus)
+		);
+
+		//////Edit////
+		const editToDo = (id, value) => {
+			const idx = toDoList.findIndex((item) => item.id === id);
+			const newToDoList = [...toDoList];
+			if (idx !== -1) {
+				newToDoList[idx] = { ...newToDoList[idx], ...value };
+			}
+			setToDoList(newToDoList);
+		};
+		/////Edit/////
+		return (
+			<div className="container">
+				<div className="mt-5 mx-auto mw-xs">
+					<AddToDo createToDo={createToDo} />
+					<SearchBar
+						searchStatus={searchStatus}
+						setSearchStatus={setSearchStatus}
+						searchText={searchText}
+						setSearchText={setSearchText}
+					/>
+					<RemainingMessage
+						remaining={pendingToDoList.length}
+						total={toDoList.length}
+					/>
+					<ToDoList
+						toDoList={filteredToDoList}
+						setToDoList={setToDoList}
+						deleteToDo={deleteToDo}
+						updateToDo={updateToDo}
+						editToDo={editToDo}
+					/>
+				</div>
 			</div>
-		</div>
-	);
-}
+		);
+	}
 
-export default App;
+	export default App;
--- src/components/TextFilter.js
+++ src/components/TextFilter.js
@@ -1,23 +1,36 @@
-function TextFilter(props) {
-	const handleChangeText = (event) => {
-		props.setSearchText(event.target.value);
-	};
+import { useState, useEffect } from "react";
 
-	return (
-		<div className="input-group">
-			<input
-				type="text"
-				className="form-control rounded-0"
-				onChange={handleChangeText}
-				value={props.searchText}
-			/>
-			<button
-				className="btn btn-secondary rounded-0"
-				onClick={() => props.setSearchText}
-			>
-				<i className="fas fa-times"> </i>
-			</button>
-		</div>
-	);
+function TextFilter({ setSearchText }) {
+ const [input, setInput] = useState("");
+
+ useEffect(() => {
+  const timeoutId = setTimeout(() => {
+   setSearchText(input);
+   console.log("input changed");
+  }, 1500);
+  return () => {
+   clearTimeout(timeoutId);
+  };
+ }, [input, setSearchText]);
+
+ const handleChangeText = event => {
+  setInput(event.target.value);
+ };
+
+ return (
+  <div className="input-group">
+   <input
+    type="text"
+    className="form-control rounded-0"
+    onChange={handleChangeText}
+    value={input}
+   />
+   <button
+    className="btn btn-secondary rounded-0"
+    onClick={() => setSearchText}>
+    <i className="fas fa-times"> </i>
+   </button>
+  </div>
+ );
 }
 export default TextFilter;
